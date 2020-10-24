4696746ce8109b6c14f8640edf895e9b18253532
Vanada1 <vovashvoev122300@gmail.com>
2020-10-24 23:52:46 +0700

    Начало работы с отрисовкой

--- CalculationImpedancesUI/MainForm.Designer.cs
+++ CalculationImpedancesUI/MainForm.Designer.cs
@@ -210,7 +210,7 @@
 			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.90129F));
 			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.09871F));
 			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
 			this.tableLayoutPanel4.Controls.Add(this.FrequencyTextBox, 1, 0);
 			this.tableLayoutPanel4.Controls.Add(this.FrequencyLabel, 0, 0);
 			this.tableLayoutPanel4.Controls.Add(this.CalculateButton, 2, 0);
@@ -227,7 +227,7 @@
 			this.FrequencyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.FrequencyTextBox.Location = new System.Drawing.Point(76, 8);
 			this.FrequencyTextBox.Name = "FrequencyTextBox";
-			this.FrequencyTextBox.Size = new System.Drawing.Size(159, 20);
+			this.FrequencyTextBox.Size = new System.Drawing.Size(158, 20);
 			this.FrequencyTextBox.TabIndex = 1;
 			this.FrequencyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			// 
@@ -250,7 +250,7 @@
 			this.CalculateButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.CalculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.CalculateButton.ForeColor = System.Drawing.SystemColors.Control;
-			this.CalculateButton.Location = new System.Drawing.Point(241, 3);
+			this.CalculateButton.Location = new System.Drawing.Point(240, 3);
 			this.CalculateButton.Name = "CalculateButton";
 			this.CalculateButton.Size = new System.Drawing.Size(29, 30);
 			this.CalculateButton.TabIndex = 0;
@@ -266,9 +266,9 @@
 			this.RemoveFrequencyButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.RemoveFrequencyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.RemoveFrequencyButton.ForeColor = System.Drawing.SystemColors.Control;
-			this.RemoveFrequencyButton.Location = new System.Drawing.Point(276, 3);
+			this.RemoveFrequencyButton.Location = new System.Drawing.Point(275, 3);
 			this.RemoveFrequencyButton.Name = "RemoveFrequencyButton";
-			this.RemoveFrequencyButton.Size = new System.Drawing.Size(31, 30);
+			this.RemoveFrequencyButton.Size = new System.Drawing.Size(32, 30);
 			this.RemoveFrequencyButton.TabIndex = 1;
 			this.RemoveFrequencyButton.UseVisualStyleBackColor = true;
 			this.RemoveFrequencyButton.Click += new System.EventHandler(this.RemoveFrequencyButton_Click);
---
+++ CalculationImpedancesUI/Manager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using CalculationImpedancesApp;
+
+namespace CalculationImpedancesUI
+{
+	class Manager
+	{
+		public DrawingNode TreeCircuit { get; set; }
+
+		private void FillCircuitNodes(Circuit circuit)
+		{
+			DrawingNode mainCircuitNode = new DrawingNode
+			{
+				Text = circuit.Name,
+			};
+			TreeCircuit.Nodes.Add(mainCircuitNode);
+			foreach (var subSegment in circuit.SubSegments)
+			{
+				DrawingNode subSegmentNode = new DrawingNode
+				{
+					Text = subSegment is IElement ? subSegment.ToString() : subSegment.Name,
+					Segment = subSegment
+				};
+				if (!(subSegmentNode.Segment is IElement))
+				{
+					FillTreeNode(subSegmentNode, subSegment);
+				}
+				TreeCircuit.Nodes[0].Nodes.Add(subSegmentNode);
+			}
+		}
+
+		private void FillTreeNode(DrawingNode parentNode, ISegment segment)
+		{
+			if (segment is IElement)
+			{
+				DrawingNode elementNode = new DrawingNode
+				{
+					Text = segment.ToString(),
+					Segment = segment
+				};
+				parentNode.Nodes.Add(elementNode);
+			}
+			else
+			{
+				foreach (var subSegment in segment.SubSegments)
+				{
+					DrawingNode segmentNode = new DrawingNode
+					{
+						Text = subSegment is IElement ? subSegment.ToString() : subSegment.Name,
+						Segment = subSegment
+					};
+					parentNode.Nodes.Add(segmentNode);
+					if (!(subSegment is IElement))
+					{
+						FillTreeNode(segmentNode, subSegment);
+					}
+				}
+			}
+		}
+
+		private void FindCoordinateNode()
+		{
+			foreach (DrawingNode node in TreeCircuit.Nodes)
+			{
+				node.FindCoordinate();
+			}
+		}
+
+
+	}
+}
